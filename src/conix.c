3de8cf8cfdd7d12799cefd3ceeda55d146aad207
Stan Vlad <vstan02@protonmail.com>
2021-02-03 19:00:47 +0200

    Implementing the function for addding options

--- src/conix.c
+++ src/conix.c
@@ -18,25 +18,70 @@
  */
 
 #include <malloc.h>
+#include <string.h>
 
 #include "conix.h"
+#include "map.h"
+#include "list.h"
+
+#define OPTION_DELIMIT ", "
+
+typedef struct t_ConixInfo ConixInfo;
 
 struct t_Conix {
     int argc;
     const char** argv;
+    Map* options;
+    List* info;
 };
+
+struct t_ConixInfo {
+    const char* name;
+    const char* description;
+};
+
+static char* conix_str_copy(const char*);
+static ConixInfo* conix_info_create(const char*, const char*);
 
 extern Conix* conix_create(int argc, const char** argv) {
     Conix* self = (Conix*) malloc(sizeof(Conix));
     self->argc = argc;
     self->argv = argv;
+    self->options = map_create();
+    self->info = list_create();
     return self;
 }
 
 extern void conix_destroy(Conix* self) {
-    if (self) free(self);
+    if (self) {
+        map_destroy(self->options);
+        list_destroy(self->info);
+        free(self);
+    }
 }
 
 extern void conix_run(Conix* self) {}
 
-extern void conix_add_option(Conix* self, ConixOption option) {}
+extern void conix_add_option(Conix* self, ConixOption option) {
+    list_push(self->info, conix_info_create(option.name, option.description));
+
+    char* id = strtok(conix_str_copy(option.name), OPTION_DELIMIT);
+    while (id) {
+        map_put(self->options, id, option.handler);
+        id = strtok(NULL, OPTION_DELIMIT);
+    }
+}
+
+static char* conix_str_copy(const char* string) {
+    size_t size = strlen(string);
+    char* result = malloc(size * sizeof(char));
+    strcpy(result, string);
+    return result;
+}
+
+static ConixInfo* conix_info_create(const char* name, const char* description) {
+    ConixInfo* info = (ConixInfo*) malloc(sizeof(ConixInfo));
+    info->name = conix_str_copy(name);
+    info->description = conix_str_copy(description);
+    return info;
+}
