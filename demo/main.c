a43f2aaffaac35319af201870e21e4b31c6a0f98
Stan Vlad <vstan02@protonmail.com>
2021-04-03 20:07:08 +0300

    Update CMakeLists.txt file

--- demo/main.c
+++ demo/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <conix.h>
 
 #define NAME "my_app"
@@ -5,8 +6,17 @@
 
 CnxApp app = { NAME, VERSION };
 
+void not_found(void* data) {
+    printf("Invalid option!");
+}
+
 int main(int argc, const char** argv) {
     CnxCli* cli = cnx_cli_init(app);
+
+    cnx_cli_add(cli, 1, (CnxOption[]) {
+        { "*", NULL, not_found, NULL }
+    });
+
     cnx_cli_run(cli, argc, argv);
     cnx_cli_free(cli);
     return 0;
