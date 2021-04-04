920e40d0c93f13eafcb8eb46b6681bd6649ebba6
Stan Vlad <vstan02@protonmail.com>
2021-04-04 19:40:53 +0300

    Updated demo

--- demo/main.c
+++ demo/main.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <conix.h>
 
-CnxApp app = { "my_app", "0.1.0" };
+CnxApp app = { "my_app", "3.1.1" };
 
-void def_option(void* data) {
-    printf("Welcome to %s!\n", (char*)data);
+void index_option(void* payload) {
+    printf("Welcome to %s!\n", (char*)payload);
 }
 
-void not_found(void* data) {
-    printf("Invalid option!");
+void about_option(void* payload) {
+    printf("%s is a simple app created with conix!\n", (char*)payload);
+}
+
+void not_found_option(void* payload) {
+    printf("%s: Invalid option!\n", (char*)payload);
 }
 
 int main(int argc, const char** argv) {
     CnxCli* cli = cnx_cli_init(app);
 
-    cnx_cli_add(cli, 2, (CnxOption[]) {
-        { "--default", "Default option", def_option, (void*)app.name },
-        { "*", NULL, not_found, NULL }
+    cnx_cli_add(cli, 3, (CnxOption[]) {
+        { "--default", "Default option", index_option, (void*)app.name },
+        { "-a, --about", "Display something", about_option, (void*)app.name },
+        { "*", NULL, not_found_option, (void*)app.name }
     });
 
     cnx_cli_run(cli, argc, argv);
