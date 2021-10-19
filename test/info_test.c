f6e2cfb30c8bc26dee9c6e7e05820290bd19f711
Stan Vlad <vstan02@protonmail.com>
2021-10-20 00:30:12 +0300

    Refactored info tests

--- test/info_test.c
+++ test/info_test.c
@@ -18,36 +18,133 @@
  */
 
 #include <glib.h>
+#include <string.h>
 
 #include "test.h"
 #include "info.h"
 
-static void test_info_main(void);
+static void test_order_of_items(void);
+static void test_containing_of_strange_items(void);
+static void test_descriptions(void);
+static void test_copying_of_items(void);
+static void test_length(void);
+
+static const char* get_desc(const info_t*, const char*);
 
 extern void add_info_tests(void) {
-    g_test_add_func(TEST_INFO_PATH, test_info_main);
+    g_test_add_func(TEST_INFO_PATH "/should_contain_items_in_correct_order", test_order_of_items);
+    g_test_add_func(TEST_INFO_PATH "/should_copy_the_name_and_description", test_copying_of_items);
+    g_test_add_func(TEST_INFO_PATH "/should_not_contain_strange_items", test_containing_of_strange_items);
+    g_test_add_func(TEST_INFO_PATH "/should_contain_correct_descriptions", test_descriptions);
+    g_test_add_func(TEST_INFO_PATH "/should_have_correct_length", test_length);
 }
 
-static void test_info_main(void) {
+static void test_order_of_items(void) {
     info_t info;
-    info_item_t items[] = {
-        { "3", "Third item" },
-        { "2", "Second item" },
-        { "1", "First item" }
-    };
-
     info_init(&info);
 
-    foreach(i, 0, 3) {
-        info_put(&info, items[i]);
-    }
+    size_t size = 3;
+    info_item_t items[] = {
+        { "third", "Third description" },
+        { "second", "Second description" },
+        { "first", "First description" }
+    };
 
-    size_t index = info.length;
+    foreach(index, 0, size) {
+        info_put(&info, items[index]);
+    }
     info_foreach(info, item, {
-        --index;
-        g_assert_cmpstr(item.name, ==, items[index].name);
-        g_assert_cmpstr(item.description, ==, items[index].description);
+        g_assert_cmpstr(item.name, ==, items[--size].name);
     });
 
     info_free(&info);
+}
+
+static void test_copying_of_items(void) {
+    info_t info;
+    info_init(&info);
+
+    char desc[] = "Test description";
+    info_put(&info, (info_item_t) { "test", desc });
+
+    char* old_desc = strdup(desc);
+    desc[2] = 'x';
+    g_assert_cmpstr(get_desc(&info, "test"), !=, desc);
+    g_assert_cmpstr(get_desc(&info, "test"), ==, old_desc);
+
+    info_free(&info);
+}
+
+static void test_descriptions(void) {
+    info_t info;
+    info_init(&info);
+
+    size_t size = 3;
+    info_item_t items[] = {
+        { "third", "Third description" },
+        { "second", "Second description" },
+        { "first", "First description" }
+    };
+
+    foreach(index, 0, size) {
+        info_put(&info, items[index]);
+    }
+    info_foreach(info, item, {
+        g_assert_cmpstr(item.description, ==, items[--size].description);
+    });
+
+    size = info.length;
+    items[1] = (info_item_t) { "second", "Second description" };
+    info_put(&info, items[1]);
+    info_foreach(info, item, {
+        g_assert_cmpstr(item.description, ==, items[--size].description);
+    });
+
+    info_free(&info);
+}
+
+static void test_containing_of_strange_items(void) {
+    info_t info;
+    info_init(&info);
+
+    g_assert_null(get_desc(&info, "first"));
+    g_assert_null(get_desc(&info, "second"));
+    g_assert_null(get_desc(&info, "third"));
+
+    info_put(&info, (info_item_t) { "first", "First item" });
+    info_put(&info, (info_item_t) { "second", "Second description" });
+    info_put(&info, (info_item_t) { "third", "Third description" });
+
+    g_assert_null(get_desc(&info, "test"));
+    g_assert_null(get_desc(&info, "First"));
+    g_assert_null(get_desc(&info, "First description"));
+
+    info_free(&info);
+}
+
+static void test_length(void) {
+    info_t info;
+    info_init(&info);
+
+    g_assert_cmpint(info.length, ==, 0);
+
+    info_put(&info, (info_item_t) { "first", "First item" });
+    info_put(&info, (info_item_t) { "second", "Second description" });
+    info_put(&info, (info_item_t) { "third", "Third description" });
+    g_assert_cmpint(info.length, ==, 3);
+
+    info_put(&info, (info_item_t) { "first", "Another second description" });
+    info_put(&info, (info_item_t) { "third", "Another third description" });
+    g_assert_cmpint(info.length, ==, 3);
+
+    info_free(&info);
+}
+
+static const char* get_desc(const info_t* info, const char* id) {
+    info_foreach(*info, item, {
+        if (!strcmp(item.name, id)) {
+            return item.description;
+        }
+    });
+    return NULL;
 }
