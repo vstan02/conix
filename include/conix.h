d53ff741a3eb4e45a94a521bb95a294022a324a4
Stan Vlad <vstan02@protonmail.com>
2022-06-07 23:01:01 +0300

    Added some documentation comments

--- include/conix.h
+++ include/conix.h
@@ -46,11 +46,36 @@ struct cnx_option {
     void* payload;
 };
 
+/**
+ * @brief Initializes a conix cli object
+ * @param app A cli configuration
+ * @return The conix cli object
+ */
 extern cnx_cli_t* cnx_cli_init(cnx_app_t app);
+
+/**
+ * @brief Deallocates the memory previously allocated by a call to cnx_cli_init
+ * @param cli A conix cli object
+ * @return Void
+ */
 extern void cnx_cli_free(cnx_cli_t* cli);
 
+/**
+ * @brief Handles passed command line arguments
+ * @param cli The conix cli object
+ * @param argc The number of command line arguments passed by the user, including the name of the program
+ * @param argv An array of character pointers listing all the arguments
+ * @return Void
+ */
 extern void cnx_cli_run(cnx_cli_t* cli, size_t argc, const char* argv[]);
 
+/**
+ * @brief Adds some options to the conix cli for handling
+ * @param cli The conix cli object
+ * @param count The count of options that should be added
+ * @param options An array of conix options
+ * @return Void
+ */
 extern void cnx_cli_add(cnx_cli_t* cli, size_t count, cnx_option_t options[]);
 
 #endif // CONIX_CONIX_H
